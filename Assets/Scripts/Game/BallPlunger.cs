bf68067cef6308c3776ccada85d00e2830dd63ad
SergioUr <sergioa.uriber@gmail.com>
2022-01-14 22:07:13 -0500

    Power restarts on collision with plunger.

--- Assets/Scripts/Game/BallPlunger.cs
+++ Assets/Scripts/Game/BallPlunger.cs
@@ -41,6 +41,7 @@ public class BallPlunger : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (!collision.gameObject.TryGetComponent<Ball>(out var ball)) return;
+        UpdatePower(0f);
         this.ball = ball;
     }
 
